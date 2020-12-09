5bdb28c3d6daeb68d70f1f77894b5db21c87451b
jylescoad-ward <jyles@dariox.club>
2020-12-09 21:07:16 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,46 +1,25 @@
 const sleep = require('util').promisify(setTimeout)
 
-async function asyncForEach (array, callback) {
-	for (let index = 0; index < array.length; index++) {
-		callback(array[index], index, array)
-	}
-}
-
-async function testOBJNum(obj) {
-	if (obj == 1) {
+var testArray = [
+	async () => {
+		// slow one
+		// sleep is emulating a http response, because its pretty fucking slow.
 		await sleep(1500);
-		return obj
-	} else {
-		return obj
-	}
-}
-
-var testOBJ = [
-	1,
-	2,
-	3,
-	4,
+		return '1';
+	},
+	() => {
+		return '2'
+	},
 ]
 
-var testLoop = async ()=>{
-	console.debug('start')
-	//for (const item of testOBJ) {
-		const promises = testOBJ.map(testOBJNum)
-		var outOBJ = await Promise.all(promises)
-		console.debug(outOBJ)
-	//}
-	console.debug('end')
-}
-testLoop();
 
-/*
-asyncForEach(["1","2","3"],async (obj)=>{
-	if (obj == 1) {
-		await setTimeout(()=>{
-			console.debug(obj)
-		},1500)
-	} else {
-		console.debug(obj)
+var testFunction = async () => {
+	console.log('start')
+	for (const obj of testArray) {
+		const returnValue = await obj();
+		console.debug(returnValue);
 	}
-})
-*/
+	console.log('end')
+}
+
+testFunction();
