b22c7ab5584302dba71ac701be58c20b54143529
jylescoad-ward <jyles@dariox.club>
2020-12-09 21:16:39 +0800

    killUpdate using queue

--- modules/apexTab/cacheRefresh.js
+++ modules/apexTab/cacheRefresh.js
@@ -12,10 +12,11 @@ module.exports.all = () => {
 	queue.add(module.exports.refreshRemoteCache)
 	queue.add(module.exports.killUpdate)
 	var loopFunction = async () => {
-		queue.start((output)=>{
-			//console.debug(`[cacheRefresh] ${output.storage.items.length} items took ${(output.storage.queueDuration/1000).toFixed(2)} seconds`)
-			loopFunction();
-		});
+		setTimeout(()=>{
+			queue.start(()=>{
+				loopFunction();
+			});
+		},SB.prefrences.apex.intervalTimeout)
 	}
 	loopFunction();
 }
@@ -42,48 +43,52 @@ module.exports.killUpdate = ()=>{
 		var tempAccounts = [];
 		var done = false;
 		var doneIndex = 0;
-		var doneLimit = temp.data.linkedUsers.length
-		await asyncForEach(temp.data.linkedUsers,async (i)=>{
+		var doneLimit = temp.data.linkedUsers.length;
+		var queue = new customQueue();
+		await asyncForEach(temp.data.linkedUsers,(i)=>{
 			if (!i.allReady) {
-				var t_res = await api.fetchUser(i.apexID)
-				if (t_res.error) {
-					throw t_res;
-				}
-				if (t_res.data.global == undefined) return;
-				var objectEntries = Object.entries(t_res.data.legends.all);
-				var entiresIndex = 0;
-				var entiresLength = objectEntries.length;
-				objectEntries.forEach((e)=>{
-					if (e[1].data != undefined) {
-						entiresIndex++;
+				queue.add(async ()=>{
+					var t_res = await api.fetchUser(i.apexID)
+					if (t_res.error) {
+						throw t_res;
 					}
-				})
-				i.allReady = false;
-				if (entiresIndex == entiresLength) {
-					// yo they actually selected all legends, a gold star for you!
-					i.allReady = true;
-				} else {
-					var allLegends = {};
-					Object.entries(i.initalData.legends.all).forEach((f)=>{
-						if (f[1].data == undefined) {
-							objectEntries.forEach((g)=>{
-								if (f[0] == g[0]) {
-									// if this is a new legend push the data we got.
-									allLegends[f[0]] = g[1];
-								}
-							})
-						} else {
-							// if this is a legend we already have keep the data we have in store.json
-							allLegends[f[0]] = f[1];
+					if (t_res.data.global == undefined) return;
+					var objectEntries = Object.entries(t_res.data.legends.all);
+					var entiresIndex = 0;
+					var entiresLength = objectEntries.length;
+					objectEntries.forEach((e)=>{
+						if (e[1].data != undefined) {
+							entiresIndex++;
 						}
 					})
-					i.initalData.legends.all = allLegends
-					i.initalData = t_res.data;
-				}
-				tempAccounts.push(i);
-				doneLimit++;
+					i.allReady = false;
+					if (entiresIndex == entiresLength) {
+						// yo they actually selected all legends, a gold star for you!
+						i.allReady = true;
+					} else {
+						var allLegends = {};
+						Object.entries(i.initalData.legends.all).forEach((f)=>{
+							if (f[1].data == undefined) {
+								objectEntries.forEach((g)=>{
+									if (f[0] == g[0]) {
+										// if this is a new legend push the data we got.
+										allLegends[f[0]] = g[1];
+									}
+								})
+							} else {
+								// if this is a legend we already have keep the data we have in store.json
+								allLegends[f[0]] = f[1];
+							}
+						})
+						i.initalData.legends.all = allLegends
+						i.initalData = t_res.data;
+					}
+					tempAccounts.push(i);
+					doneLimit++;
+				})
 			}
 		})
+		await queue.start();
 		if (doneLimit === doneIndex) {
 			done = true
 		}
