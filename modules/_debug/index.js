eed493a2e9f86849b89e98473858e000144724d0
Jyles Coad-Ward <jyles@dariox.club>
2021-06-20 16:10:30 +0800

    Removed old files and put actual script in ./old/

--- modules/_debug/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = function() {
-    SB.client.on('message', async message => {
-        if (message.author.bot) return;
-        //console.log(`[${message.author.id}] - ${message.content}`)
-    })
-}
